df472d97059b0e9b2173bcdecf9b307c30e87d74
Manvendra Singh <MANVENDRAS2608@GMAIL.COM>
2024-01-13 11:38:07 +0530

    Created QB-Crafting

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+Config.Recipes = {
+    {
+        item = "veh_toolbox",
+        requiredItems = {
+            { item = "repairkit", amount = 1 },
+            { item = "cleaningkit", amount = 1 }
+        }
+    },
+    -- Add more recipes as needed
+}
