65c30698b5b53d2cf8e4c8326ae030ce5570b8f7
Manvendra Singh <MANVENDRAS2608@GMAIL.COM>
2024-01-13 11:58:35 +0530

    Updated recipe items

--- config.lua
+++ config.lua
@@ -1,10 +1,99 @@
 Config = {}
 Config.Recipes = {
     {
-        item = "veh_toolbox",
+        item = "lockpick",
         requiredItems = {
-            { item = "repairkit", amount = 1 },
-            { item = "cleaningkit", amount = 1 }
+            { item = "metalscrap", amount = 1 },
+            { item = "plastic", amount = 1 }
+        }
+    },
+    {
+        item = "screwdriverset",
+        requiredItems = {
+            { item = "metalscrap", amount = 1 },
+            { item = "plastic", amount = 1 }
+        }
+    },
+    {
+        item = "electronickit",
+        requiredItems = {
+            { item = "metalscrap", amount = 1 },
+            { item = "plastic", amount = 1 },
+            { item = "aluminum", amount = 1 }
+        }
+    },
+    {
+        item = "radioscanner",
+        requiredItems = {
+            { item = "electronickit", amount = 1 },
+            { item = "plastic", amount = 1 },
+            { item = "steel", amount = 1 }
+        }
+    },
+    {
+        item = "gatecrack",
+        requiredItems = {
+            { item = "metalscrap", amount = 1 },
+            { item = "plastic", amount = 1 },
+            { item = "aluminum", amount = 1 },
+            { item = "iron", amount = 1 },
+            { item = "electronickit", amount = 1 }
+        }
+    },
+    {
+        item = "handcuffs",
+        requiredItems = {
+            { item = "metalscrap", amount = 1 },
+            { item = "steel", amount = 1 },
+            { item = "aluminum", amount = 1 }
+        }
+    },
+    {
+        item = "repairkit",
+        requiredItems = {
+            { item = "metalscrap", amount = 1 },
+            { item = "steel", amount = 1 },
+            { item = "plastic", amount = 1 }
+        }
+    },
+    {
+        item = "pistol_ammo",
+        requiredItems = {
+            { item = "metalscrap", amount = 1 },
+            { item = "steel", amount = 1 },
+            { item = "copper", amount = 1 }
+        }
+    },
+    {
+        item = "ironoxide",
+        requiredItems = {
+            { item = "iron", amount = 1 },
+            { item = "glass", amount = 1 }
+        }
+    },
+    {
+        item = "aluminumoxide",
+        requiredItems = {
+            { item = "aluminum", amount = 1 },
+            { item = "glass", amount = 1 }
+        }
+    },
+    {
+        item = "armor",
+        requiredItems = {
+            { item = "iron", amount = 1 },
+            { item = "steel", amount = 1 },
+            { item = "plastic", amount = 1 },
+            { item = "aluminum", amount = 1 }
+        }
+    },
+    {
+        item = "drill",
+        requiredItems = {
+            { item = "iron", amount = 1 },
+            { item = "steel", amount = 1 },
+            { item = "screwdriverset", amount = 1 },
+            { item = "advancedlockpick", amount = 1 }
         }
     },
     -- Add more recipes as needed
